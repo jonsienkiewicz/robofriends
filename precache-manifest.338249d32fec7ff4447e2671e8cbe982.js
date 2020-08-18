50a17a988296c8b04dbc07afa89a7a581809df7b
Jonathan Sienkiewicz <65567533+jonsienkiewicz@users.noreply.github.com>
2020-08-18 17:40:11 -0300

    Updates

---
+++ precache-manifest.338249d32fec7ff4447e2671e8cbe982.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "deca5398aada722e9e68a185b32fadb0",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "4ed288ca7e3142d13f62",
+    "url": "/robofriends/static/css/main.32761312.chunk.css"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/robofriends/static/js/2.cab51068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.cab51068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ed288ca7e3142d13f62",
+    "url": "/robofriends/static/js/main.32f3c535.chunk.js"
+  },
+  {
+    "revision": "e7d2cbb368e0813da61d",
+    "url": "/robofriends/static/js/runtime-main.74eee7b5.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
